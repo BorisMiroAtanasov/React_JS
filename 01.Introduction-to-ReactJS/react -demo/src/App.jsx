d41fbd021cb0e51ec33ba74e4286c06d6afe9de1
BorisMiroAtanasov <borisatanasov84@abv.bg>
2024-02-28 23:49:16 +0200

    first React demo

---
+++ 01.Introduction-to-ReactJS/react -demo/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <div>
+        <h1>My First React aplication</h1>
+      
+      </div>
+      <h1>Vite + React = best combo</h1>
+      
+    </div>
+  )
+}
+
+export default App
