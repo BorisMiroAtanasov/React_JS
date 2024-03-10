ebf6e481d607b14867d571b1f6e4cf023f6d17fa
BorisMiroAtanasov <borisatanasov84@abv.bg>
2024-03-10 12:41:46 +0200

    React useEffect demo

--- 03.Components-Deep-Dive/React_JS_Componenets_deep_Dive_demo_boris/copmonent-deep-dive-boris/src/App.jsx
+++ 03.Components-Deep-Dive/React_JS_Componenets_deep_Dive_demo_boris/copmonent-deep-dive-boris/src/App.jsx
@@ -3,26 +3,46 @@ import { useState, useEffect } from "react";
 
 function App() {
     const [numbers, setNumbers] = useState([1,2,3,4,5]);
+    const [count, setCount] = useState(0);
+    const [isManualUpdate, setIsManualUpdate] = useState(false)
 
     useEffect(() =>{
         console.log('Mountin components');
         
     },[])
     useEffect(() =>{
-        console.log('Update component - numbers');
+        console.log(`Update component - ${numbers.length}`);
         
-    },[numbers])
+    },[numbers]);
+    
+    useEffect(() =>{
+        if(!isManualUpdate){
+            setTimeout(() => setCount(s => s + 1), 1000 )
+
+        }else{
+            setIsManualUpdate(false)
+        }
+        
+    },[count])
+  
 
   const onClick = () => {
-    setNumbers(oldState => oldState.slice(0 , oldState.length - 1))
-  
+    setNumbers(oldState => oldState.slice(0 , oldState.length - 1));
+    
   };
+
+  const setCountFunc = () => {
+    setCount(c => c + 1)
+    setIsManualUpdate(true)
+  }
   return (
     <div>
+        <h3>Count: {count}</h3>
       <ul>
         {numbers.map ((number, index) => <li data-key={index} key={index}> {number * 2} </li>)}
       </ul>
         <button onClick={onClick}>Remove</button>
+        <button onClick={setCountFunc}>+</button>
     </div>
   );
 }
