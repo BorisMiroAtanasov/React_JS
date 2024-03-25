c267fd3b5e9894c278fc0b48b9da0edbea9cd7dd
BorisMiroAtanasov <borisatanasov84@abv.bg>
2024-03-25 23:12:00 +0200

    Workshop 1 user list -crate user

--- 04.Workshop-components/Demo_Boris/client/src/services/userService.js
+++ 04.Workshop-components/Demo_Boris/client/src/services/userService.js
@@ -1,13 +1,13 @@
 const baseUrl = "http://localhost:3030/jsonstore/users";
 
 export const getAll = async () => {
-  try {
-    const response = await fetch(baseUrl);
-    const result = await response.json();
+    try {
+        const response = await fetch(baseUrl);
+        const result = await response.json();
 
-    const data = Object.values(result);
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+        const data = Object.values(result);
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
 };
---
+++ 04.Workshop-components/Demo_Boris/client/src/utils/dataUtils.js
@@ -0,0 +1,4 @@
+export const formatData = (isoData) =>{
+    const options = { year:'numeric', month:'long', day:'numeric'};
+    return new Date(isoData).toLocaleDateString(undefined, options)
+}
