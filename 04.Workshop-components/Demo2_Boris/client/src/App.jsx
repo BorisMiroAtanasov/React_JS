87d199b5b4bf99f63d51402684292ead465eb573
BorisMiroAtanasov <borisatanasov84@abv.bg>
2024-04-04 23:59:46 +0300

    workshop1_demo 2 - config , header, main , footer

---
+++ 04.Workshop-components/Demo2_Boris/client/src/App.jsx
@@ -0,0 +1,22 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import "./styles.css";
+
+function App() {
+  return (
+    <div>
+        <Header />
+        <main className="main">
+
+            
+
+        </main>
+
+
+        <Footer />
+
+    </div>
+  )
+}
+
+export default App;
