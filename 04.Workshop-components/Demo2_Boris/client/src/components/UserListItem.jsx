cf0b3bdc9179b0eb4ae81abfa79c3aa0a4446c81
BorisMiroAtanasov <borisatanasov84@abv.bg>
2024-04-06 23:58:33 +0300

    Workshop1 convert iso date

--- 04.Workshop-components/Demo2_Boris/client/src/components/UserListItem.jsx
+++ 04.Workshop-components/Demo2_Boris/client/src/components/UserListItem.jsx
@@ -1,3 +1,5 @@
+import { formatData } from "../services/util";
+
 export default function UserListItem({
    
     firstName,
@@ -18,7 +20,7 @@ export default function UserListItem({
         <td>{lastName}</td>
         <td>{email}</td>
         <td>{phoneNumber}</td>
-        <td>{createdAt}</td>
+        <td>{formatData(createdAt)}</td>
 
         <td className="actions">
           <button className="btn edit-btn" title="Edit">
---
+++ 04.Workshop-components/Demo2_Boris/client/src/services/util.js
@@ -0,0 +1,7 @@
+export const formatData= (isoDate)=> {
+    const options = { year:'numeric', month:'long', day:'numeric'}
+
+    return new Date(isoDate).toLocaleDateString(undefined,options)
+
+
+}
