26f1ad8ba795d9f9ae107d5218ca02f1aa568488
BorisMiroAtanasov <borisatanasov84@abv.bg>
2024-04-14 23:54:33 +0300

    Forms -demo update

--- 05.Forms/react-forms-boris/react-forms/src/components/ControlledForm.jsx
+++ 05.Forms/react-forms-boris/react-forms/src/components/ControlledForm.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
 
 export default function ControlledForm(){
-    const [userNameValue , setUserNamevalue] = useState('Ivo')
+    const [userNameValue , setUserNamevalue] = useState('');
+    const [passwordValue , setPasswordvalue] = useState('');
+    const [ageValue , setAgevalue] = useState('')
     const userNameChangeHandler = (e) => {
         setUserNamevalue(e.target.value)
 
+    }
+    const resetFormHandler = () =>{
+      setUserNamevalue('')
+    }
+
+    const passwordChangeHandler = (e) => {
+      setPasswordvalue(e.target.value)
+    }
+
+    const ageChangeHandler = (e) => {
+      setAgevalue(Number(e.target.value))
     }
     return (
         <>
@@ -19,20 +32,33 @@ export default function ControlledForm(){
           id="username" 
           value={userNameValue}
           onChange={userNameChangeHandler}
+          onBlur={() => console.log('onBlur')}
 
           />
         </div>
         <div>
           <label htmlFor="password">Password:</label>
-          <input type="password" name="password" id="password" />
+          <input 
+          type="password" 
+          name="password" 
+          id="password" 
+          value={passwordValue}
+          onChange={passwordChangeHandler}
+          />
         </div>
         <div>
           <label htmlFor="age">Age:</label>
-          <input type="number" name="age" id="age" />
+          <input 
+          type="number" 
+          name="age" 
+          id="age"
+          value={ageValue}
+          onChange={ageChangeHandler}
+           />
         </div>
         <div>
           <button>Register</button>
-          <button type="button">Reset</button>
+          <button type="button" onClick={resetFormHandler}>Reset</button>
         </div>
       </form>
     </>
