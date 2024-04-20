eb52605ca216c6680eac4672d7de8aba1ac36d0d
BorisMiroAtanasov <borisatanasov84@abv.bg>
2024-04-21 00:07:31 +0300

    Routing - demo - setup done

---
+++ 06.Routing/routing-demo/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navigation from "./components/Navigations";
+
+function App() {
+  return (
+  <>
+  <h1>React Router</h1>
+
+  <footer>All rights reserved &copy; </footer>
+    <Navigation />
+  </>
+  )
+}
+
+export default App;
---
+++ 06.Routing/routing-demo/src/components/Navigations.jsx
@@ -0,0 +1,13 @@
+const Navigation = () =>{
+    return(
+        <nav>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+
+
+
+        </nav>
+    )
+}
+
+export default Navigation
