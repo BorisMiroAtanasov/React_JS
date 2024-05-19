e095eb1ac4353ce99df6ea629bcafb1a1a8f83f6
BorisMiroAtanasov <borisatanasov84@abv.bg>
2024-05-20 00:20:15 +0300

    custom hook demo

--- 08.Reack-Hooks/I-papazov-demo/react-hooks-demo/src/App.jsx
+++ 08.Reack-Hooks/I-papazov-demo/react-hooks-demo/src/App.jsx
@@ -1,35 +1,57 @@
-import { useState, useEffect } from 'react';
-import Header from './components/Header';
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import TodoList from './components/TodoList';
-import AddTodoModal from './components/AddTodoModal';
+import "bootstrap/dist/css/bootstrap.min.css";
+import TodoList from "./components/TodoList";
+import AddTodoModal from "./components/AddTodoModal";
 
-const baseUrl = 'http://localhost:3030/jsonstore/todos'
+const baseUrl = "http://localhost:3030/jsonstore/todos";
 function App() {
-  const[todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
+  const [showAddTodo, setshowAddTodo] = useState(false)
 
   useEffect(() => {
     fetch(baseUrl)
-    .then(res => res.json())
-    .then(result => {
-      setTodos(Object.values(result))
-    })
-  },[])
+      .then((res) => res.json())
+      .then((result) => {
+        setTodos(Object.values(result));
+      });
+  }, []);
 
- 
+  const onTodoAddSubmit = async (values) => {
+    //console.log(values);
+    const response = await fetch(baseUrl, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(values),
+    });
+    const result = await response.json();
+    setshowAddTodo(false)
+    setTodos(state => [...state, result])
+
+
+    console.log(result);
+  };
+
+  const onTodoAddClick = () =>{
+    setshowAddTodo(true)
+
+  }
+  const onTodoAddClose =() => {
+    setshowAddTodo(false)
+  }
 
   return (
     <>
-    <Header />
+      <Header />
 
-    <TodoList  todos={todos}/>
-    
-    <AddTodoModal />
+      <TodoList todos={todos} onTodoAddClick={onTodoAddClick} />
 
-      
+      <AddTodoModal show={showAddTodo} onTodoAddSubmit={onTodoAddSubmit} onTodoAddClose={onTodoAddClose} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 08.Reack-Hooks/I-papazov-demo/react-hooks-demo/src/components/AddTodoModal.jsx
+++ 08.Reack-Hooks/I-papazov-demo/react-hooks-demo/src/components/AddTodoModal.jsx
@@ -1,44 +1,50 @@
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
-import { useState } from 'react';
-import { useForm } from '../hooks/useForm';
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
+import { useForm } from "../hooks/useForm";
 
+export default function AddTodoModal({ onTodoAddSubmit, show ,onTodoAddClose}) {
+  const { formValues, onChangeHandler, onSubmit } = useForm(
+    {
+      text: "",
+    },
+    onTodoAddSubmit
+  );
 
-export default function AddTodoModal() {
-    const {formValues , onChangeHandler,onSubmit} = useForm({
-        text: '',
-    }, (values)=>{
-        console.log(values);
-    })
+  // const onTodoSubmit =(e) =>{
+  //  e.preventDefault()
+  //  console.log(formValues);
+  // }
+  return (
+    <Modal show={show} onEscapeKeyDown={onTodoAddClose}>
+      <Modal.Header closeButton onHide={onTodoAddClose}>
+        <Modal.Title>Add todo</Modal.Title>
+      </Modal.Header>
 
-    
-
-    // const onTodoSubmit =(e) =>{
-    //  e.preventDefault()
-    //  console.log(formValues);
-    // }
-    return (
-        <Modal show={true}>
-        <Modal.Header closeButton>
-          <Modal.Title>Add todo</Modal.Title>
-        </Modal.Header>
-
-        <Modal.Body>
+      <Modal.Body>
         <Form onSubmit={onSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Todo</Form.Label>
-        <Form.Control type="text" name='text' placeholder="Do the dishes" value={formValues.name}  onChange={onChangeHandler}/>
-       
-      </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Todo</Form.Label>
+            <Form.Control
+              type="text"
+              name="text"
+              placeholder="Do the dishes"
+              value={formValues.name}
+              onChange={onChangeHandler}
+            />
+          </Form.Group>
 
- 
-      <Button variant="primary" type="submit" style={{marginRight :'10px'}}>
-        Submit
-      </Button>
-          <Button variant="secondary">Close</Button>
-    </Form>
-        </Modal.Body>
-      </Modal>
-    )
+          <Button
+            variant="primary"
+            type="submit"
+            style={{ marginRight: "10px" }}
+          >
+            Submit
+          </Button>
+          <Button variant="secondary" onClick={onTodoAddClose}>Close</Button>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
 }
