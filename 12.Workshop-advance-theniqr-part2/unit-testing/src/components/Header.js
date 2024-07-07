9caa3a41c59a7c6c949be16a9050f1c50b63f3e8
BorisMiroAtanasov <borisatanasov84@abv.bg>
2024-07-07 11:17:15 +0300

    unit test lab

---
+++ 12.Workshop-advance-theniqr-part2/unit-testing/src/components/Header.js
@@ -0,0 +1,18 @@
+export default function Header(){
+    return (
+        <>
+        <h1>Unit Testing</h1>
+
+        <nav>
+       <a href="/">Home</a>
+       <a href="/about">About</a>
+
+        </nav>
+
+
+        
+        
+        
+        </>
+    )
+}
